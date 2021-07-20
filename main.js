91b79c123c7c5d744a5699494bf5c4f3cf13e370
AarushSarda <77834111+AarushSarda@users.noreply.github.com>
2021-07-20 18:48:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-nose_x = 0;
-nose_y = 0;
-
 function preload() {
-     clown_nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+
 }
 function setup() {
      canvas = createCanvas(300,300);
@@ -16,10 +13,8 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        nose_x = results[0].pose.nose.x-15;
-        nose_y = results[0].pose.nose.y-15;
-        console.log("Nose x =  " + nose_x);
-        console.log("Nose y =  " + nose_y);
+        console.log("Nose x =  " + results[0].pose.nose.x);
+        console.log("Nose y =  " + results[0].pose.nose.y);
     }
 }
 function modelLoaded() {
@@ -27,9 +22,7 @@ function modelLoaded() {
 }
 function draw() {
     image(video , 0 , 0 , 300 , 300);
-    image(clown_nose , nose_x , nose_y , 30 , 30)
 }
 function takeSnapshot() {
-    save("I am a joker.png")
+    save("I have a mustach.png")
 }
-
